9c890d7bebde842e5b2496652641aba50d25f9b4
EVerton Reis <itsevertonreis@gmail.com>
2024-11-12 19:03:54 -0300

    chore: atualizar configurações do Prisma e preparar variáveis de ambiente para Mailtrap

--- my-app/src/app/services/auth/index.ts
+++ my-app/src/app/services/auth/index.ts
@@ -1,7 +1,34 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
- 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+import NextAuth from 'next-auth'
+import EmailProvider from 'next-auth/providers/nodemailer'
+
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '../database/index'
+
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  pages: {
+    signIn: '/auth',
+    signOut: '/auth',
+    error: '/auth',
+    verifyRequest: '/auth',
+    newUser: '/app',
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  events: {
+    createUser: async (message) => {
+      await createStripeCustomer({
+        name: message.user.name as string,
+        email: message.user.email as string,
+      })
+    },
+  },
 })
